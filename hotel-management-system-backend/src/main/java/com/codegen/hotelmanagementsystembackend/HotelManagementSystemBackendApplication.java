49af5c915211af811bc0cae3c03c4d0f4041a923
Keshan <janindupathi@gmail.com>
2024-02-26 21:49:14 +0530

    Commit with the package structure

---
+++ hotel-management-system-backend/src/main/java/com/codegen/hotelmanagementsystembackend/HotelManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codegen.hotelmanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementSystemBackendApplication.class, args);
+	}
+
+}
