0670994f291da2238575d5be9b2e42b304b8cb31
Keshan <janindupathi@gmail.com>
2024-05-10 21:48:43 +0530

    Updated

--- hotel-management-system-frontend/src/app/main/customer/components/hotel-details-context/inner-items/hotel-overview/hotel-overview.component.ts
+++ hotel-management-system-frontend/src/app/main/customer/components/hotel-details-context/inner-items/hotel-overview/hotel-overview.component.ts
@@ -9,4 +9,35 @@ export class HotelOverviewComponent{
 
   @Input() hotelDetails: any;
 
+  // Variables to track the visibility of accordion items
+  isCollapseOneVisible: boolean = true;
+  isCollapseTwoVisible: boolean = false;
+  isCollapseThreeVisible: boolean = false;
+  isCollapseFourVisible: boolean = false;
+  rotationAngle: number = 0;
+  rotationAngles: number[] = [0, 0, 0, 0];
+
+  toggleCollapse(collapseNumber: number) {
+    switch (collapseNumber) {
+      case 1:
+        this.isCollapseOneVisible = !this.isCollapseOneVisible;
+        break;
+      case 2:
+        this.isCollapseTwoVisible = !this.isCollapseTwoVisible;
+        break;
+      case 3:
+        this.isCollapseThreeVisible = !this.isCollapseThreeVisible;
+        break;
+      case 4:
+        this.isCollapseFourVisible = !this.isCollapseFourVisible;
+        break;
+      default:
+        break;
+    }
+  }
+
+  toggleRotation(index: number) {
+    this.rotationAngles[index] = this.rotationAngles[index] === 0 ? -180 : 0;
+  }
+
 }
